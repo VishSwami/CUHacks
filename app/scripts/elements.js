e2fe400ebef2bd238af34cd513b708b1b95bfb13
VishSwami <swvishal.s@gmail.com>
2015-06-21 06:06:54 -0700

    Update elements.js

--- app/scripts/elements.js
+++ app/scripts/elements.js
@@ -44,4 +44,49 @@ var elements = module.exports = [
     shells: 2,
     'electro-negativity': 'high'
   }
+  {
+    name: 'Magnesium',
+    abbr: 'Mg',
+    mass: 24.3,
+    protons: 12,
+    group: 'nonmetal',
+    shells: 2,
+    'electro-negativity': 'low'
+  }
+  {
+    name: 'Potassium',
+    abbr: 'K',
+    mass: 39.1,
+    protons: 19,
+    group: 'nonmetal',
+    shells: 4,
+    'electro-negativity': 'low'
+  }
+  {
+    name: 'Sodium',
+    abbr: 'Na',
+    mass: 22.99,
+    protons: 11,
+    group: 'nonmetal',
+    shells: 3,
+    'electro-negativity': 'low'
+  }
+  {
+    name: 'Iron',
+    abbr: 'Fe',
+    mass: 55.85,
+    protons: 26,
+    group: 'metal',
+    shells: 4,
+    'electro-negativity': 'medium'
+  }
+  {
+    name: 'Chlorine',
+    abbr: 'Cl',
+    mass: 335.45,
+    protons: 17,
+    group: 'nonmetal',
+    shells: 3,
+    'electro-negativity': 'high'
+  }
 ]
